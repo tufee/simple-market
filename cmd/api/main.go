6564703920f597c0d6ace1b9f4659ff2ce9fb936
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2025-02-23 13:08:15 -0300

    refactor main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,10 +7,8 @@ import (
 	"net/http"
 
 	_ "modernc.org/sqlite"
-
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/sqlite3"
-	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"simple.market/internal/api"
+	"simple.market/pkg/utils"
 )
 
 func main() {
@@ -20,12 +18,10 @@ func main() {
 	}
 	defer db.Close()
 
-	if err := applyMigrations(db); err != nil {
+	if err := utils.ApplyMigrations(db); err != nil {
 		log.Fatal("Error applying migrations:", err)
 	}
-	router := http.NewServeMux()
-
-	router.HandleFunc("GET /hello", Hello)
+	router := api.SetupRoutes()
 
 	server := http.Server{
 		Addr:    ":3000",
@@ -34,30 +30,4 @@ func main() {
 
 	fmt.Println("Server listening on port 3000")
 	server.ListenAndServe()
-}
-
-func Hello(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-}
-
-func applyMigrations(db *sql.DB) error {
-	driver, err := sqlite3.WithInstance(db, &sqlite3.Config{})
-	if err != nil {
-		return err
-	}
-
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://../../migrations",
-		"sqlite", driver)
-
-	if err != nil {
-		return err
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		return err
-	}
-
-	fmt.Println("Migrations applied successfully!")
-	return nil
 }
