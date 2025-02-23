a36676bd0570a080137903db54bd47ccf569344d
Paulo Almeida <pauloalmeida.jrr@gmail.com>
2025-02-23 16:54:09 -0300

    add validate utils

---
+++ pkg/utils/validate_utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "regexp"
+
+func IsValidEmail(email string) bool {
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	return regexp.MustCompile(emailRegex).MatchString(email)
+}
